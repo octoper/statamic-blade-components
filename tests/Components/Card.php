a47a61b0b884ecad7f15fcd1bfdecf0eb8935f70
Vaggelis Yfantis <octoper@users.noreply.github.com>
2021-03-26 16:51:36 +0000

    Apply fixes from StyleCI

--- tests/Components/Card.php
+++ tests/Components/Card.php
@@ -4,15 +4,15 @@ namespace Octoper\BladeComponents\Tests\Components;
 
 use Illuminate\View\Component;
 
-
-class Card extends Component {
-	public function render()
-	{
-		return <<<'blade'
+class Card extends Component
+{
+    public function render()
+    {
+        return <<<'blade'
 			<div class="card">
 				<h1>{{ $title }}</h1>
 				<div>{{ $slot }}</div>
 			</div>
 		blade;
-	}
+    }
 }
--- tests/TagTest.php
+++ tests/TagTest.php
@@ -5,71 +5,69 @@ use Octoper\BladeComponents\Tests\TestCase;
 uses(TestCase::class);
 
 test('simple hey component', function () {
-	$component = renderAntler('{{ component:hey }}');
+    $component = renderAntler('{{ component:hey }}');
 
-	expect($component)->toBeRendered(
-		<<<EOF
+    expect($component)->toBeRendered(
+        <<<'EOF'
 		<div>Hey</div>
 		EOF
-	);
+    );
 });
 
-
 test('simple hey component with name attribute', function () {
-	$component = renderAntler('{{ component:hey name="Nick" }}');
+    $component = renderAntler('{{ component:hey name="Nick" }}');
 
-	expect($component)->toBeRendered(
-		<<<EOF
+    expect($component)->toBeRendered(
+        <<<'EOF'
 		<div>Hey Nick</div>
 		EOF
-	);
+    );
 });
 
 test('section component with default slot', function () {
-	$component = renderAntler(
-		<<<EOF
+    $component = renderAntler(
+        <<<'EOF'
 			{{ component:section name="Main" }}
 				Main Section
 			{{ /component:section }}
 		EOF
-	);
+    );
 
-	expect($component)->toBeRendered(
-		<<<EOF
+    expect($component)->toBeRendered(
+        <<<'EOF'
 			<div>
 				<h1>Main</h1>
 				<div>Main Section</div>
 			</div>
 		EOF
-	);
+    );
 });
 
 test('section component with default slot and a Statamic variable', function () {
-	$component = renderAntler(
-		<<<EOF
+    $component = renderAntler(
+        <<<'EOF'
 			{{ component:section name="{name}" }}
 				{{ name }} Section
 			{{ /component:section }}
 		EOF,
-		[
-			"name" => "Main"
-		]
-	);
+        [
+            'name' => 'Main',
+        ]
+    );
 
-	expect($component)->toBeRendered(
-		<<<EOF
+    expect($component)->toBeRendered(
+        <<<'EOF'
 			<div>
 				<h1>Main</h1>
 				<div>Main Section</div>
 			</div>
 		EOF
-	);
+    );
 });
 
-
 test('card component with title slot', function () {
-	$component = renderAntler(
-		<<<EOF
+    $component = renderAntler(
+        <<<'EOF'
 			{{ component:card }}
 				<x-slot name="title">
 					Hey
@@ -78,14 +76,14 @@ test('card component with title slot', function () {
 				Hello
 			{{ /component:card }}
 		EOF
-	);
+    );
 
-	expect($component)->toBeRendered(
-		<<<EOF
+    expect($component)->toBeRendered(
+        <<<'EOF'
 			<div class="card">
 				<h1>Hey</h1>
 				<div>Hello</div>
 			</div>
 		EOF
-	);
+    );
 });
